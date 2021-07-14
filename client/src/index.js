42c6a1e306299afb27d37fed42135b2da082fcfc
John Hinojosa <hinojohn@ymail.com>
2021-07-14 01:36:37 -0500

    testing for production -auth0 environment values

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain =
-  process.env.REACT_APP_AUTH0_DOMAIN || "wanderingadventure.us.auth0.com";
-const clientId =
-  process.env.REACT_APP_AUTH0_CLIENT_ID || "OK1FHdAl6buDevVYFZ3dnfksZDwIhEka";
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <Auth0Provider
